35fa03d46908f9fb6dc866311508dc108c9acf71
Mehmet Burkay Kıraç <kiracmehmetburkay@gmail.com>
2024-09-17 20:44:40 +0300

    feat: Frontend add polygon has finished

--- OpenLayers-Project-Frontend/src/App.jsx
+++ OpenLayers-Project-Frontend/src/App.jsx
@@ -5,10 +5,44 @@ import View from "ol/View.js";
 import { Draw, Modify, Snap } from "ol/interaction.js";
 import { OSM, Vector as VectorSource } from "ol/source.js";
 import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer.js";
-import { get, fromLonLat } from "ol/proj.js";
-import { React, useEffect } from "react";
-
+import { Feature } from "ol";
+import { Polygon } from "ol/geom";
+import { get, fromLonLat, toLonLat } from "ol/proj.js";
+import { React, useEffect, useState, useRef } from "react";
+import { Menubar } from "primereact/menubar";
+import { Dialog } from "primereact/dialog";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
 function App() {
+  const [visible, setVisible] = useState(false);
+  const [map, setMap] = useState(null);
+  const [newPolygonId, setNewPolygonId] = useState("");
+  const [newPolygonName, setNewPolygonName] = useState("");
+  const [isadd, setIsAdd] = useState(false);
+  const [newCoordinates, setNewCoordinates] = useState(null);
+  const [newSnap, setNewSnap] = useState(null);
+  const [dummyData, setDummyData] = useState([]);
+
+
+  const items = [
+    {
+      label: "Add Drawing",
+      icon: "pi pi-pencil",
+      command: (event) => {
+        addInteractions(event);
+      },
+    },
+    {
+      label: "Query Drawing",
+      icon: "pi pi-server",
+      command: (event) => {
+        source.removeFeatures();
+      },
+    },
+  ];
+
+  const mapRef = useRef();
+
   const raster = new TileLayer({
     source: new OSM(),
   });
@@ -29,45 +63,112 @@ function App() {
   extent[0] += extent[0];
   extent[2] += extent[2];
 
-  let draw, snap;
+  let draw, vectorLayer, vectorSource;
 
-  function addInteractions(map) {
+  function addInteractions() {
     draw = new Draw({
       source: source,
       type: "Polygon",
     });
 
     map.addInteraction(draw);
-  
-    // Çizim bittiğinde tetiklenen olay
-    draw.on('drawend', function (event) {
-      const polygon = event.feature.getGeometry();
-      const coordinates = polygon.getCoordinates();
-  
-      console.log('Polygon Koordinatları:', coordinates);
+    draw.on("drawend", function (event) {
+      setVisible(true);
+      var geometry = event.feature.getGeometry();
+
+      var coordinates = geometry.getCoordinates();
+
+      var wgs84Coordinates = coordinates[0].map(function (coord) {
+        return ol.proj.transform(coord, "EPSG:3857", "EPSG:4326");
+      });
+      setNewCoordinates(wgs84Coordinates);
+      //const snap = new Snap({ source: source });
+      //setNewSnap(snap);
     });
-  
-    snap = new Snap({ source: source });
-    map.addInteraction(snap);
   }
   useEffect(() => {
-    const map = new Map({
-      layers: [raster, vector],
+    vectorSource = new VectorSource();
+
+    // Create features from dummy data
+    dummyData.forEach((polygon) => {
+      const feature = new Feature({
+        geometry: new Polygon([
+          polygon.coordinates.map((coord) => fromLonLat(coord)),
+        ]),
+      });
+      feature.setProperties({ name: polygon.name, id: polygon.id });
+      vectorSource.addFeature(feature);
+    });
+
+    vectorLayer = new VectorLayer({
+      source: vectorSource,
+    });
+
+    const map2 = new Map({
       target: "map",
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+        vectorLayer,
+      ],
       view: new View({
-        center: fromLonLat([32.8597, 39.9334]),
+        center: fromLonLat([35.866287, 38.925533]),
         zoom: 6,
-        extent,
       }),
     });
-    const modify = new Modify({ source: source });
-    map.addInteraction(modify);
+    setMap(map2);
+    return () => map2.setTarget(undefined);
+  }, [dummyData]);
+ 
 
-    addInteractions(map);
-  });
   return (
     <>
+      <div className="card">
+        <Menubar model={items} />
+      </div>
       <div id="map" className="map"></div>
+      <Dialog
+        header="Add New Polygon"
+        visible={visible}
+        position="top"
+        onHide={() => {
+          if (!visible) return;
+          setVisible(false);
+        }}
+        draggable={false}
+        resizable={false}
+      >
+        <label style={{ marginRight: "50px" }}>Id: </label>
+        <InputText
+          value={newPolygonId}
+          onChange={(e) => setNewPolygonId(e.target.value)}
+        />
+        <br />
+        <br />
+        <label style={{ marginRight: "20px" }}>Name: </label>
+        <InputText
+          value={newPolygonName}
+          onChange={(e) => setNewPolygonName(e.target.value)}
+        />
+        <br />
+        <br />
+        <Button
+          label="Add"
+          icon="pi pi-plus"
+          style={{ marginLeft: "100px" }}
+          onClick={() => {
+            //map.addInteraction(newSnap);
+            const newPolygon = {
+              id: newPolygonId,
+              name: newPolygonName,
+              coordinates: newCoordinates,
+            };
+            setDummyData(prevPolygons => [...prevPolygons, newPolygon]);
+            console.log(dummyData);
+          }}
+        />
+      </Dialog>
     </>
   );
 }
--- OpenLayers-Project-Frontend/src/main.jsx
+++ OpenLayers-Project-Frontend/src/main.jsx
@@ -2,7 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import "primereact/resources/themes/mdc-dark-indigo/theme.css";
+import "primeicons/primeicons.css";
 createRoot(document.getElementById('root')).render(
-    <App />
+    <PrimeReactProvider>
+<App />
+    </PrimeReactProvider>
+    
 )
